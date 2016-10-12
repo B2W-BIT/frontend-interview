36f01d522bdd1c87172ccabc3cd17b81226a3bdb
Fernando Souza <fernando.souza@liferay.com>
2016-10-12 20:53:21 -0300

    Merge with test server

--- test/integration/app.js
+++ test/integration/app.js
@@ -1,62 +1,59 @@
-describe('Route Books', () => {
-  describe('Profile information', () => {
-    describe('Route GET /twitter/user', () => {
-      it('should return profile data', (done) => {
-        request
-          .get('/twitter/user')
-          .end((err, res) => {
-            expect(res.body).to.be.an('object');
-            expect(res.body.screen_name).to.be.eql('americanascom');
-            expect(res.body.description).to.be.exist;
+describe('Profile information', () => {
+  describe('Route GET /twitter/user', () => {
+    it('should return profile data', (done) => {
+      request
+        .get('/twitter/user')
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body.screen_name).to.be.eql('americanascom');
+          expect(res.body.description).to.be.exist;
 
-            done(err);
-          });
-      });
+          done(err);
+        });
+    });
 
-      it('should return the url of the bigger profile image', (done) => {
-        request
-          .get('/twitter/user')
-          .end((err, res) => {
-            expect(res.body.profile_image_url_bigger).to.be.a('string');
+    it('should return the url of the bigger profile image', (done) => {
+      request
+        .get('/twitter/user')
+        .end((err, res) => {
+          expect(res.body.profile_image_url_bigger).to.be.a('string');
 
-            done(err);
-          });
-      });
+          done(err);
+        });
+    });
 
-      it('should return the formated created at date', (done) => {
-        request
-          .get('/twitter/user')
-          .end((err, res) => {
-            expect(res.body.created_at_formated).to.be.eql('April 2009');
+    it('should return the formated created at date', (done) => {
+      request
+        .get('/twitter/user')
+        .end((err, res) => {
+          expect(res.body.created_at_formated).to.be.eql('April 2009');
 
-            done(err);
-          });
-      });
+          done(err);
+        });
     });
   });
+});
 
-  describe('Tweets', () => {
-    describe('Route GET /twitter/tweets', () => {
-      it('should return a list with ten tweets', (done) => {
-        request
-          .get('/twitter/tweets')
-          .end((err, res) => {
-            expect(res.body.length).to.be.eql(10);
+describe('Tweets', () => {
+  describe('Route GET /twitter/tweets', () => {
+    it('should return a list with ten tweets', (done) => {
+      request
+        .get('/twitter/tweets')
+        .end((err, res) => {
+          expect(res.body.length).to.be.eql(10);
 
-            done(err);
-          });
-      });
+          done(err);
+        });
+    });
 
-      it('should return a list of tweets from americanas.com', (done) => {
-        request
-          .get('/twitter/tweets')
-          .end((err, res) => {
-            expect(res.body[0].user.screen_name).to.be.eql('americanascom');
+    it('should return a list of tweets from americanas.com', (done) => {
+      request
+        .get('/twitter/tweets')
+        .end((err, res) => {
+          expect(res.body[0].user.screen_name).to.be.eql('americanascom');
 
-            done(err);
-          });
-      });
+          done(err);
+        });
     });
   });
-
 });
