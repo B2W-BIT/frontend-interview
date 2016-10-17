1abff95cb6d0a5f94dc51982e4119677d49c5b0f
felipemartins81 <felipemartins81@gmail.com>
2016-10-17 14:44:25 -0200

    Challenge Timeline v0

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'POhO6wrsp6jKgOSCMpqbHZc5S');
+    define('CONSUMER_SECRET', 'D91S7yHzGhTPymF5iEfzJ2r93WR71YGX9uoEqkYwVCx3EhsvQc');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '390275385-Ptarbl4ZEsSerW7zDd9tj0tjDgAg8921qoT13a0X');
+    define('ACCESS_SECRET', 'egm2hFhKslHkLY1mNcZCHY4WydqzBdU6lx58etbTUz81o');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
