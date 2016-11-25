a5e97dc786e75dd8c481af0130c7514a21e4074f
Vinícius Lagêdo <vinicius@hadrons.io>
2016-11-25 02:14:25 -0200

    Initial Structure

---
+++ brunch-config.js
@@ -0,0 +1,39 @@
+exports.config = {
+  overrides: {
+    production: {
+      plugins: {
+        off: ['eslint-brunch'],
+      },
+    },
+  },
+  conventions: {
+    ignored: [
+      /[\\/]_/,
+      /\-test.js$/,
+    ],
+  },
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/vendor.js': /^node_modules/,
+        'js/app.js': /^app/,
+      },
+    },
+    stylesheets: {
+      joinTo: 'styles/app.css',
+    },
+  },
+  plugins: {
+    sass: {
+      mode: "native"
+    },
+    babel: {
+      babelrc: true,
+    },
+  },
+  modules: {
+    autoRequire: {
+      'js/app.js': ['js/app'],
+    },
+  },
+};
