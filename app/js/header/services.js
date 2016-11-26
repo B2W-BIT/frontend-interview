b0de3142902034ebf69e344d087ca804c78cf848
Vinicius Lagêdo <viniciuslagedo@gmail.com>
2016-11-26 04:56:52 -0200

    Service com constant

--- app/js/header/services.js
+++ app/js/header/services.js
@@ -1,3 +1,3 @@
 import * as HTTP from '../utils/network'
-const baseUrl = 'http://localhost:3000'
-export const getUserInfos = () => HTTP.get(`${baseUrl}/users/show`)
+import { APIRoot} from '../utils/constants'
+export const getUserInfos = () => HTTP.get(`${APIRoot}/users/show`)
