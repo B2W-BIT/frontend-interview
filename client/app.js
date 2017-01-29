475f0d67dac3d6acec9d9ed805147cf08389470e
Yuri Ramos <yuri.pramos@gmail.com>
2017-01-28 22:03:45 -0200

    first commit, stream working

---
+++ client/app.js
@@ -0,0 +1,28 @@
+/* eslint-env browser */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from'./components/Header.js'
+
+import TweetList from './components/TweetList';
+
+
+var initialState = JSON.parse(document.getElementById('initial-state').innerHTML);
+
+export default class Main extends React.Component{
+
+
+	render(){
+		return(
+			    <div>
+			    	<Header/>
+				     <TweetList data={initialState} />
+				</div> 
+			);
+	}
+}
+
+
+ReactDOM.render(<Main />,document.getElementById('root'));
+
+
---
+++ client/components/Header.js
@@ -0,0 +1,20 @@
+
+import io from 'socket.io-client';
+
+import React from 'react';
+import Tweet from './Tweet';
+
+
+
+export default class Header extends React.Component {
+
+	 render() {
+    	return (
+    			<header>
+	    			<div>
+	    				<p> oi</p>
+	    			</div>
+    			</header>
+    		);
+    }
+}
