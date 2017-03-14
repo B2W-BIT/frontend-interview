e29ba8892037244f1ea9f50aaaa68fbcb48bff7a
Mario Mariani <marioslmariani@gmail.com>
2017-03-14 15:54:43 -0300

    Load header and avatar from Twitter

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -1,5 +1,19 @@
 import './header.css';
 
+class HeaderController {
+  constructor(twitter) {
+    twitter
+      .getUser('americanascom')
+      .then(response => {
+        this.user = response.data;
+        this.user.profile_image_url_400x400 =
+          this.user.profile_image_url.replace('_normal', '_400x400');
+      });
+  }
+}
+
 export const header = {
-  template: require('./header.html')
+  template: require('./header.html'),
+  controller: HeaderController,
+  controllerAs: 'headerCtrl'
 };
